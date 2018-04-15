7589a50c3ce7c4d0f6e7b16b2c5f2c76a1eabe94
YJ <good.dba.yj@gmail.com>
2018-04-15 18:05:15 +0900

    nova_status.py

---
+++ nova_status.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import sys
+import socket
+from os import environ as env
+from novaclient.client import Client
+​
+def get_nova_credentials_v2():
+       d = {}
+       d['version'] = '2'
+       d['username'] = 'openstack_dba'
+       d['password'] = 'password'
+       d['auth_url'] ='https://**.**.**.**:13000/v2.0'
+       d['project_id'] ='94c95a8ce5c945eb8340391f17f7f7e8'
+       d['cacert'] ='/engn001/masterha/openstack_scripts/overcloud-cacert.pem'
+       return d
+​
+credentials = get_nova_credentials_v2()
+nova_client = Client(**credentials)
+servers_list = nova_client.servers.list()
+server_tostop = sys.argv[1]
+server_exists = False
+server_status = ""
+​
+try:
+   socket.inet_aton(server_tostop)
+   for s in servers_list:
+      #use Cloud Network's configuration name 
+       tmp = s.networks['20180415_100']
+       if server_tostop == tmp[0] :
+           server = s
+           server_exists = True
+           break
+except socket.error:
+   for s in servers_list:
+       if s.name == server_tostop :
+           server = s
+           server_exists = True
+           break
+​
+if not server_exists:
+   print("server %s does not exist" % server_tostop)
+   exit("unknown")
+else:
+   exit(server.status)
